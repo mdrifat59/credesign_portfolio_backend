9d1ae404b1a819a8b2dc1579356c0ce443d89141
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-02 12:59:30 +0600

    banner api  route done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 var cors = require('cors')
 const mongoose = require('mongoose');
 const Navbar = require('./model/navbarModel')
+const Banner = require('./model/bannerModel')
 
 
 mongoose.connect('mongodb+srv://mdrifatulislam59:ON86VIVvkh1oAjSF@cluster0.2ckl6wm.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0')
@@ -29,10 +30,27 @@ app.put('/navbar/:id', function (req, res) {
   Navbar.findByIdAndUpdate(req.params.id, req.body).then(() => {
     res.send({ message: "Navbar updated" })
   })
-}) 
+})
 // Navbar route end
 // Banner route start
+app.post('/banner', function (req, res) {
+  const data = new Banner(req.body)
+  data.save()
+  res.send("banner created")
+  console.log(req);
 
+})
+
+app.get('/banneritem', async function (req, res) {
+  const data = await Banner.findOne({})
+  res.send(data)
+})
+
+app.put('/banner/:id', function (req, res) {
+  Banner.findByIdAndUpdate(req.params.id, req.body).then(() => {
+    res.send({ massage: "banner update" })
+  })
+})
 // Banner route end
 
 app.listen(8000)
